b6a300847436217a4b7faa18f383343b7918808c
William Webb <webbdevvue@gmail.com>
2020-06-04 15:20:07 -0400

    added a few styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import './App.css'
 
 
 //setting up the original data for the App to work with
