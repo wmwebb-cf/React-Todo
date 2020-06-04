f57011dc66f89c3dd83137e6a8a02cd5263f16b0
William Webb <webbdevvue@gmail.com>
2020-06-03 20:47:15 -0400

    Initial commit - Todo and TodList components are working

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+    console.log(props.list);
+    return (
+        <div className='TodoList'>
+            {props.list.map(item => (
+                <Todo key={item.id} task={item.task} completed={item.completed} />
+            ))}
+        </div>
+    )}
+
+
+export default TodoList;
